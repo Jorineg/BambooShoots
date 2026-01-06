53805faa252a90d862f713e66f011d0826b788e4
Jorin Eggers <jorineggers@gmail.com>
2026-01-07 00:35:19 +0100

    update sanity version

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
+import { media } from 'sanity-plugin-media'
 import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
@@ -12,7 +13,8 @@ export default defineConfig({
 
     plugins: [
         structureTool(),
-        visionTool()
+        visionTool(),
+        media()
     ],
 
     schema: {
